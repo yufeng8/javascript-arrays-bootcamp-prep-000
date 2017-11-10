c1eeef89e5d810d056b117143e13fd90af125be7
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 19:02:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(A, N) {
+  return A.unshift(N)
+}
+
+function destructivelyAddElementToBeginningOfArray(A,N) {
+  return  [N, ...A]
+}
